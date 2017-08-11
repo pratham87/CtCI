61cc2eed339d3c0934d9d063b1fed3cb75b7c169
Prathamesh Kawale <kawa5@live.com>
2017-08-10 19:30:47 -0700

    PalindromePermutation

---
+++ src/main/java/arraysAndStrings/Q4/PalindromePermutation.java
@@ -0,0 +1,36 @@
+package arraysAndStrings.Q4;
+
+/*
+ Question: Check if a string is a permutation of a palindrome. 
+ 
+ Algorithm: 
+ 1. Get the count of every char.
+ 2. Filter the odd integers and if more than 1 return false.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PalindromePermutation {
+
+	public static boolean isPalindromeOfPermutation(String s) {
+		s = s.toLowerCase();
+		s = s.replaceAll("\\s", "");
+		Map<Character, Integer> map = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			map.put(s.charAt(i), map.containsKey(s.charAt(i)) ? map.get(s.charAt(i)) + 1 : 1);
+		}
+
+		if (map.values().stream().filter(v -> v % 2 != 0).collect(Collectors.toList()).size() > 1) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isPalindromeOfPermutation("Tact Coa"));
+	}
+
+}
